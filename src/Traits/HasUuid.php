b8bff17d4b304e1561d58c8151e67dee8d0f574d
Laravel Utility <larautil@outlook.com>
2023-11-06 22:48:38 +0800

    add HasUuid trait

---
+++ src/Traits/HasUuid.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * |----------------------------------------------------------------------------
+ * | Use uuid as secondary key and route key
+ * |----------------------------------------------------------------------------
+ */
+
+namespace LaraUtil\Foundation\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+    protected static string $uuidColumnName = 'uuid';
+
+    protected static bool $useUuidAsRouteKey = true;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{self::$uuidColumnName} = Str::orderedUuid()->toString();
+        });
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return self::$useUuidAsRouteKey ? self::$uuidColumnName : $this->getKeyName();
+    }
+}
